294fb4278009fb8cdf177d739d871911616d068b
huubach1822 <monsterxyz02@gmail.com>
2024-02-10 01:01:17 +0700

    before sequelize

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import initApiRoutes from "./routes/api";
 import 'dotenv/config'
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT
 
 initApiRoutes(app);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
     console.log("Running: " + PORT);
